85d1370e95950ef6f848746dfe6ba0335eddbf95
ab <a@b.com>
2024-01-07 20:56:47 +0800

    updated

--- official_train_test.py
+++ official_train_test.py
@@ -11,11 +11,6 @@ from torchmetrics.functional.regression import mean_absolute_error
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
 
-# define any number of nn.Modules (or use your current ones)
-encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
-decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
-
-
 # define the LightningModule
 class LitAutoEncoder(L.LightningModule):
     def __init__(self, encoder, decoder):
@@ -59,26 +54,39 @@ class LitAutoEncoder(L.LightningModule):
         self.log("val_loss", val_loss)
         self.log("val_MAE", val_mae)
 
+    def forward(self, x):
+        return self.decoder(self.encoder(x.view(x.size(0), -1)))
 
-# init the autoencoder
-autoencoder = LitAutoEncoder(encoder, decoder)
+encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
 
-transform = transforms.ToTensor()
+def train_test():
+    # define any number of nn.Modules (or use your current ones)
+    # init the autoencoder
+    autoencoder = LitAutoEncoder(encoder, decoder)
 
-train_set = datasets.MNIST(root="./", download=True, train=True, transform=transform)
-# use 20% of training data for validation
-train_set_size = int(len(train_set) * 0.8)
-valid_set_size = len(train_set) - train_set_size
-# split the train set into two
-seed = torch.Generator().manual_seed(42)
-train_set, valid_set = data.random_split(train_set, [train_set_size, valid_set_size], generator=seed)
-train_loader = DataLoader(train_set, batch_size=64)
-valid_loader = DataLoader(valid_set, batch_size=64)
-test_set = datasets.MNIST(root="./", download=True, train=False, transform=transform)
-test_loader = DataLoader(test_set, batch_size=64)
+    transform = transforms.ToTensor()
+
+    train_set = datasets.MNIST(root="./", download=True, train=True, transform=transform)
+    # use 20% of training data for validation
+    train_set_size = int(len(train_set) * 0.8)
+    valid_set_size = len(train_set) - train_set_size
+    # split the train set into two
+    seed = torch.Generator().manual_seed(42)
+    train_set, valid_set = data.random_split(train_set, [train_set_size, valid_set_size], generator=seed)
+    train_loader = DataLoader(train_set, batch_size=64)
+    valid_loader = DataLoader(valid_set, batch_size=64)
+    test_set = datasets.MNIST(root="./", download=True, train=False, transform=transform)
+    test_loader = DataLoader(test_set, batch_size=64)
+
+    # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+    trainer = L.Trainer(limit_train_batches=200, max_epochs=2, callbacks=[EarlyStopping(monitor="val_MAE", mode="min")])
+    trainer.fit(model=autoencoder, train_dataloaders=train_loader, val_dataloaders=valid_loader)
+    trainer.test(model=autoencoder, dataloaders=test_loader)
+
+def main():
+    train_test()
 
 
-# train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
-trainer = L.Trainer(limit_train_batches=200, max_epochs=2, callbacks=[EarlyStopping(monitor="val_MAE", mode="min")])
-trainer.fit(model=autoencoder, train_dataloaders=train_loader, val_dataloaders=valid_loader)
-trainer.test(model=autoencoder, dataloaders=test_loader)
+if __name__ == "__main__":
+    main()
--- predict.py
+++ predict.py
@@ -1,9 +1,11 @@
 import torch
 from official_train_test import LitAutoEncoder, encoder, decoder
 
-model=LitAutoEncoder.load_from_checkpoint("./lightning_logs/version_9/checkpoints/epoch=1-step=400.ckpt", encoder=encoder, decoder=decoder)
-model.eval()
-inp = torch.randn(1,1,28,28)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+auto_encoder=LitAutoEncoder.load_from_checkpoint("./lightning_logs/version_9/checkpoints/epoch=1-step=400.ckpt", encoder=encoder, decoder=decoder)
+auto_encoder.eval()
+inp = torch.randn(1,1,28,28).to(device)
 with torch.no_grad():
-    out = model(inp)
+    #out = auto_encoder.decoder(auto_encoder.encoder(inp.view(inp.size(0), -1)))
+    out = auto_encoder(inp.view(inp.size(0), -1))
 print(out.shape)
